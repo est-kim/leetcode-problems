b94b5c961c22fb1c5f48ec92af207f6e4bdf48b1
Esther Kim <kim.esther117@gmail.com>
2023-02-12 22:54:21 -0800

    Solved validAnagram JavaScript

---
+++ validAnagram/validAnagram.js
@@ -0,0 +1,30 @@
+// Given two strings s and t, return true if t
+// is an anagram of s, and false otherwise
+
+var isAnagram = function(s, t) {
+    if (s.length !== t.length) {
+        return false;
+    }
+    let obj1 = {};
+    let obj2 = {};
+    for (let i of s) {
+        if (obj1[i] === undefined) {
+            obj1[i] = 1;
+        } else {
+            obj1[i] += 1;
+        }
+    }
+    for (let j of t) {
+        if (obj2[j] === undefined) {
+            obj2[j] = 1;
+        } else {
+            obj2[j] += 1;
+        }
+    }
+    for (let key in obj1) {
+        if (obj1[key] !== obj2[key]) {
+            return false;
+        }
+    }
+    return true;
+};
