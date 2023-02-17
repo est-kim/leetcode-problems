5e87b71804abdb57133ab58814f2003221ed5d8a
Esther Kim <kim.esther117@gmail.com>
2023-02-17 12:53:00 -0800

    solved plusOne in JavaScript

---
+++ plusOne/plusOne.js
@@ -0,0 +1,17 @@
+// You are given a large integer represented as an
+// integer array digits, where each digits[i] is the ith
+// digit of the integer. The digits are ordered from most
+// significant to least significant in left-to-right order.
+// The large integer does not contain any leading 0's.
+
+// Increment the large integer by one and return the
+// resulting array of digits.
+
+var plusOne = function(digits) {
+    let nextNum = (BigInt(digits.join('')) + 1n).toString();
+    let newArr = []
+    for (let i = 0; i < nextNum.length; i++) {
+        newArr.push(parseInt(nextNum[i]))
+    }
+    return newArr
+};
