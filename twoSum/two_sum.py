ed14ed730d72065adba2a33e95a6cb5e582b596b
Esther Kim <kim.esther117@gmail.com>
2023-03-13 22:17:46 -0700

    two_sum in python

---
+++ twoSum/two_sum.py
@@ -0,0 +1,16 @@
+# Given an array of integers nums and an integer
+# target, return indices of the two numbers such
+# that they add up to target.
+
+# You may assume that each input would have exactly
+# one solution, and you may not use the same element
+# twice.
+
+# You can return the answer in any order.
+
+def two_sum(nums, target):
+    obj = {}
+    for i, num in enumerate(nums):
+        if target - num in obj:
+            return [obj[target - num], i]
+        obj[num] = i
