c72ab5740dee5388f98a7c50cf894cc06878c0d3
Esther Kim <kim.esther117@gmail.com>
2023-04-04 20:48:13 -0700

    longestSubstring

---
+++ longestSubstring/longestSubstring.js
@@ -0,0 +1,12 @@
+// Given a string s, find the length of the longest
+// substring without repeating characters.
+
+var lengthOfLongestSubstring = function(s) {
+    let start = 0;
+    let index = 0;
+    for (let next = 1; next < s.length; next++) {
+        if (s[next] === s[index]) {
+            
+        }
+    }
+};
