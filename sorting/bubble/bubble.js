5bbcfcceb486646789c82fe7a47411448bb9efd9
Esther Kim <kim.esther117@gmail.com>
2023-04-09 13:41:19 -0700

    optimized bubbleSort in JavaScript

--- sorting/bubble/bubble.js
+++ sorting/bubble/bubble.js
@@ -1,15 +1,17 @@
 const bubbleSort = (arr) => {
-    const swap = (arr, idx1, idx2) => {
-        [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
-    }
-
-    for (let i = arr.length; i > 0; i--) {
-        for (let j = 0; j < i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                swap(arr, j, j + 1);
-            }
-        }
-    }
-    
-    return arr;
+	const swap = (arr, index1, index2) => {
+		[arr[index1], arr[index2]] = [arr[index2], arr[index1]];
+	}
+	let noSwaps;
+	for (let i = arr.length; i > 0; i--) {
+		noSwaps = true;
+		for (let j = 0; j < i - 1; j++) {
+			if (arr[j] > arr[j + 1]) {
+				noSwaps = false;
+				swap(arr, j, j + 1);
+			}
+		}
+		if(noSwaps) break;
+	}
+	return arr;
 }
