d449816a59060606f8a143a89b6818336bd40d51
Esther Kim <kim.esther117@gmail.com>
2023-04-28 21:02:38 -0700

    minDifference description

--- arraysAndHashing/minDifference/minDifference.js
+++ arraysAndHashing/minDifference/minDifference.js
@@ -1,4 +1,14 @@
+// You are given a 0-indexed integer array nums, where nums[i]
+// represents the score of the ith student. You are also given
+// an integer k.
 
+// Pick the scores of any k students from the array so that
+// the difference between the highest and the lowest of the
+// k scores is minimized.
+
+// Return the minimum possible difference.
+
+// source: https://leetcode.com/problems/minimum-difference-between-highest-and-lowest-of-k-scores/
 
 var minimumDifference = function(nums, k) {
     const n = nums.length;
