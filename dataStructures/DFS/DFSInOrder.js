7ec3c29f1f0fe43789354ecd5f6110c92528d6d6
Esther Kim <kim.esther117@gmail.com>
2023-05-08 21:31:29 -0700

    DFS In Order Iterative Method

--- dataStructures/DFS/DFSInOrder.js
+++ dataStructures/DFS/DFSInOrder.js
@@ -1,79 +1,97 @@
 class Node {
-    constructor(value) {
-      this.value = value;
-      this.left = null;
-      this.right = null;
-    }
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
   }
+}
 
-  class BinarySearchTree {
-    constructor() {
-      this.root = null;
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+  insert(value) {
+    var newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
     }
-    insert(value) {
-      var newNode = new Node(value);
-      if (this.root === null) {
-        this.root = newNode;
-        return this;
-      }
-      var current = this.root;
-      while (true) {
-        if (value === current.value) return undefined;
-        if (value < current.value) {
-          if (current.left === null) {
-            current.left = newNode;
-            return this;
-          }
-          current = current.left;
-        } else {
-          if (current.right === null) {
-            current.right = newNode;
-            return this;
-          }
-          current = current.right;
+    var current = this.root;
+    while (true) {
+      if (value === current.value) return undefined;
+      if (value < current.value) {
+        if (current.left === null) {
+          current.left = newNode;
+          return this;
         }
-      }
-    }
-    find(value) {
-      if (this.root === null) return false;
-      var current = this.root,
-        found = false;
-      while (current && !found) {
-        if (value < current.value) {
-          current = current.left;
-        } else if (value > current.value) {
-          current = current.right;
-        } else {
-          found = true;
+        current = current.left;
+      } else {
+        if (current.right === null) {
+          current.right = newNode;
+          return this;
         }
+        current = current.right;
       }
-      if (!found) return undefined;
-      return current;
-    }
-    contains(value) {
-      if (this.root === null) return false;
-      var current = this.root,
-        found = false;
-      while (current && !found) {
-        if (value < current.value) {
-          current = current.left;
-        } else if (value > current.value) {
-          current = current.right;
-        } else {
-          return true;
-        }
-      }
-      return false;
-    }
-    DFSInOrder(){
-        let data = [];
-        let current = this.root;
-        function traverse(node) {
-            if (node.left) traverse(node.left);
-            data.push(node.value);
-            if (node.right) traverse(node.right);
-        }
-        traverse(current);
-        return data;
     }
   }
+  find(value) {
+    if (this.root === null) return false;
+    var current = this.root,
+      found = false;
+    while (current && !found) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        found = true;
+      }
+    }
+    if (!found) return undefined;
+    return current;
+  }
+  contains(value) {
+    if (this.root === null) return false;
+    var current = this.root,
+      found = false;
+    while (current && !found) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        return true;
+      }
+    }
+    return false;
+  }
+  DFSInOrder() {
+    let data = [];
+    let current = this.root;
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      data.push(node.value);
+      if (node.right) traverse(node.right);
+    }
+    traverse(current);
+    return data;
+  }
+}
+
+// Iterative method
+const DFSInOrder2 = (root) => {
+  let stack = [];
+  let traversed = [];
+  let curr = root;
+
+  while (stack.length || curr) {
+    while (curr) {
+      stack.push(curr);
+      curr = curr.left;
+    }
+    curr = stack.pop();
+    traversed.push(curr.val);
+    curr = curr.right;
+  }
+  return traversed;
+};
