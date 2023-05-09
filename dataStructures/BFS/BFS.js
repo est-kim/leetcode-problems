c3d2143e5cc2fd6adc8325748003cc7ed208652b
Esther Kim <kim.esther117@gmail.com>
2023-05-08 20:50:38 -0700

    BFS in JavaScript

---
+++ dataStructures/BFS/BFS.js
@@ -0,0 +1,82 @@
+class Node {
+    constructor(value){
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinarySearchTree {
+    constructor(){
+        this.root = null;
+    }
+    insert(value){
+        var newNode = new Node(value);
+        if(this.root === null){
+            this.root = newNode;
+            return this;
+        }
+        var current = this.root;
+        while(true){
+            if(value === current.value) return undefined;
+            if(value < current.value){
+                if(current.left === null){
+                    current.left = newNode;
+                    return this;
+                }
+                current = current.left;
+            } else {
+                if(current.right === null){
+                    current.right = newNode;
+                    return this;
+                }
+                current = current.right;
+            }
+        }
+    }
+    find(value){
+        if(this.root === null) return false;
+        var current = this.root,
+            found = false;
+        while(current && !found){
+            if(value < current.value){
+                current = current.left;
+            } else if(value > current.value){
+                current = current.right;
+            } else {
+                found = true;
+            }
+        }
+        if(!found) return undefined;
+        return current;
+    }
+    contains(value){
+        if(this.root === null) return false;
+        var current = this.root,
+            found = false;
+        while(current && !found){
+            if(value < current.value){
+                current = current.left;
+            } else if(value > current.value){
+                current = current.right;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+    BFS(){
+      let queue = [];
+      let data = [];
+      let current = this.root;
+      queue.push(current);
+
+      while (queue.length !== 0) {
+        current = queue.shift(); // removes first element from queue array and returns that element
+        data.push(current.value)
+        if (current.left) queue.push(current.left);
+        if (current.right) queue.push(current.right)
+      }
+      return data;
+    }
+}
