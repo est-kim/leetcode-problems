c45363f7b6fad26834b6637aae696b5ac985080f
Esther Kim <kim.esther117@gmail.com>
2023-07-01 01:19:47 -0700

    solved

--- codeWars/arrays/dominator/dominator.js
+++ codeWars/arrays/dominator/dominator.js
@@ -1,3 +1,17 @@
 // source: https://www.codewars.com/kata/559e10e2e162b69f750000b4/train/javascript
 
+function dominator(arr) {
+  const obj = {};
+
+  for (let i = 0; i < arr.length; i++) {
+    obj[arr[i]] = (obj[arr[i]] || 0) + 1;
+  }
+
+  for (let key in obj) {
+    if (obj[key] > arr.length / 2) {
+      return parseInt(key);
+    }
+  }
+  return -1;
+}
 
---
+++ codeWars/linkedLists/removeDuplicates/removeDuplicates.js
@@ -0,0 +1,42 @@
+// Linked Lists - Remove Duplicates
+
+// Write a RemoveDuplicates() function which takes a list sorted in increasing order and
+// deletes any duplicate nodes from the list. Ideally, the list should only be traversed once.
+// The head of the resulting list should be returned.
+
+// source: https://www.codewars.com/kata/55d9f257d60c5fd98d00001b
+
+function Node(data) {
+  this.data = data;
+  this.next = null;
+}
+
+function removeDuplicates(head) {
+  if (head === null) return null;
+
+  // set curr node as head
+  let curr = head;
+  // while curr isn't null and curr.next isnt null,
+  while (curr !== null && curr.next !== null) {
+    // set check node to curr
+    let checkingNode = curr;
+    // while check node isn't null
+    while (checkingNode.next !== null) {
+      // if check node's val.next is the same as curr node.val, duplicate is found
+      if (checkingNode.next.data === curr.data) {
+        // link check node.next to next next
+        checkingNode.next = checkingNode.next.next;
+      } else {
+        // else, move to next node
+        checkingNode = checkingNode.next;
+      }
+    }
+    // curr = curr.next
+    curr = curr.next;
+  }
+  // return head
+  return head;
+}
+
+// time complexity: O(n^2)
+// space complexity: O(1)
